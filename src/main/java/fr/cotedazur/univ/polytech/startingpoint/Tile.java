23e805cb14d17ecf69f381757edbcede6eaf1575
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2022-12-05 16:09:04 +0100

    refactored isNeighbour and added getNeighbourCoorinates in Tile #16

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Tile.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Tile.java
@@ -22,18 +22,7 @@ public class Tile {
     //tests to see if the tile to test is neighbour to this tile
     //check coordinate system at : https://www.redblobgames.com/grids/hexagons/#neighbors-axial
     public boolean isNeighbour (Tile tileToTest) {
-        ArrayList<Tile> neighboursToTest = new ArrayList<>();   //this ArrayList gets all the neighbour position of this tile
-        //attribution of neighbourToTest
-        for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) {
-                if (i != j) {   //gets rid of (-1, -1), (+0, +0), (+1, +1)
-                    int xToTest = coordinnateX + i;
-                    int yToTest = coordinnateY + j;
-                    Tile potentialNeighbour = new Tile(xToTest, yToTest);
-                    neighboursToTest.add(potentialNeighbour);
-                }
-            }
-        }
+        ArrayList<Tile> neighboursToTest = this.getNeighbourCoordinates();
         for (int i = 0; i < 6; i++) {
             boolean sameX = tileToTest.getCoordinnateX() == neighboursToTest.get(i).getCoordinnateX();
             boolean sameY = tileToTest.getCoordinnateY() == neighboursToTest.get(i).getCoordinnateY();
@@ -41,16 +30,34 @@ public class Tile {
                 return true;
             }
         }
-
-        /*
-        //if you want to check the values that are processed
-        System.out.println(neighboursToTest.size());
-        for (int i = 0; i < 6; i++) {
-            System.out.println(neighboursToTest.get(i));
-        }*/
-
         return false;
     }
+
+    //returns the neighbour tiles of this tile
+    public ArrayList<Tile> getNeighbourCoordinates () {
+        ArrayList<Tile> neighbourCoordinates = new ArrayList<>();   //this ArrayList gets all the neighbour position of this tile
+        //attribution of neighbourCoordinates
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (i != j) {   //gets rid of (-1, -1), (+0, +0), (+1, +1)
+                    int xToTest = coordinnateX + i;
+                    int yToTest = coordinnateY + j;
+                    Tile potentialNeighbour = new Tile(xToTest, yToTest);
+                    neighbourCoordinates.add(potentialNeighbour);
+                }
+            }
+        }
+
+        /*
+        //check the result
+        System.out.println(neighbourCoordinates.size());
+        for (int i = 0; i < 6; i++) {
+            System.out.println(neighbourCoordinates.get(i));
+        }*/
+
+        return neighbourCoordinates;
+    }
+
 
     @Override
     public String toString() {
