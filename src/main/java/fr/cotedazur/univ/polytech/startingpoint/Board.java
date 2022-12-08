206b5db478dcfdc1d9a948d48c9d1719177891b8
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2022-12-08 13:04:49 +0100

    added a method that scans the available positions for new tiles in the board #16

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Board.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Board.java
@@ -10,10 +10,34 @@ public class Board {
         return "Une carte à été posée en:"+tile.getCoordinnateX()+" "+tile.getCoordinnateY();
     }
 
-    //this method returns an ArrayList of all the possible positions where a new tile can be added
+    //this method returns an ArrayList of all the possible positions that are in contact with the edge of the board
+    //TODO : make it return only the available positions for new tiles = coordinates where there are at least two neighbours
     public ArrayList<Coordinate> scanAvailableTilePosition() {
-        //TODO
-        return new ArrayList<Coordinate>();
+        ArrayList<Coordinate> potentiallyAvailableCoordinates = new ArrayList<>();
+        for (int i = 0; i < boardTiles.size(); i++) {   //creates an array that contains all the coordinates of the tiles of the board and the available tile positions at the edge of the board BUT contains double coordinates
+            potentiallyAvailableCoordinates.addAll(boardTiles.get(i).getNeighbourCoordinates());
+        }
+        ArrayList<Coordinate> availableCoordinates = new ArrayList<>();
+        for (int i = 0; i < potentiallyAvailableCoordinates.size(); i ++) {
+            boolean isDouble = false;
+            boolean isPlaced = false;
+            for (int j = 0; j < availableCoordinates.size(); j++) {
+                if (potentiallyAvailableCoordinates.get(i).equals(availableCoordinates.get(j))) {
+                    isDouble = true;
+                    break;
+                }
+            }
+            for (int j = 0; j < boardTiles.size(); j++) {
+                if (potentiallyAvailableCoordinates.get(i).equals(boardTiles.get(j).getCoordinate())) {
+                    isPlaced = true;
+                    break;
+                }
+            }
+            if (!isDouble && !isPlaced) {
+                availableCoordinates.add(potentiallyAvailableCoordinates.get(i));
+            }
+        }
+        return availableCoordinates;
     }
 
 }
