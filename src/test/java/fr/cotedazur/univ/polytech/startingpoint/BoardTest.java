583cf9dbfebd2494d609330ec260ea93ca195f8c
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2022-12-08 13:29:55 +0100

    refactored test for method scanAvailableTilePosition in Board.java #16

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/BoardTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/BoardTest.java
@@ -2,28 +2,39 @@ package fr.cotedazur.univ.polytech.startingpoint;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
     Board twoTiles = new Board();
     Tile t0_0 = new Tile(0,0);
-    Tile t0_1 = new Tile(0,1);
+    Tile t1_0 = new Tile(1,0);
 
     @BeforeEach
     void setup() {
         twoTiles.addTile(t0_0);
-        twoTiles.addTile(t0_1);
+        twoTiles.addTile(t1_0);
     }
 
-    @Test
+    @Test   //tests for the fisrt version, the one that only requires new tiles to be touching the edge, not the ones having two neighbours
     void testScanAvailableTilePosition() {
         ArrayList<Coordinate> tilesToTest = twoTiles.scanAvailableTilePosition();
+        ArrayList<Coordinate> trueTiles = new ArrayList<>();
 
+        trueTiles.add(new Coordinate(-1, 0));
+        trueTiles.add(new Coordinate(-1, 1));
+        trueTiles.add(new Coordinate(0, -1));
+        trueTiles.add(new Coordinate(0, 1));
+        trueTiles.add(new Coordinate(1, -1));
+        trueTiles.add(new Coordinate(1, 1));
+        trueTiles.add(new Coordinate(2, -1));
+        trueTiles.add(new Coordinate(2, 0));
+
+        assertEquals(8, tilesToTest.size());
         for (int i = 0; i < tilesToTest.size(); i++) {
-            System.out.println(tilesToTest.get(i));
+            assertTrue(tilesToTest.get(i).equals(trueTiles.get(i)));
+            //System.out.println(tilesToTest.get(i));   //only to visualise the ArrayList
         }
     }
 }
