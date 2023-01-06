ae0fb4c720198f64359055022b9525c31ff44cb7
Simon Beurel <100567717+simonbeurel@users.noreply.github.com>
2023-01-06 16:15:13 +0100

    Update GameEngine.java

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/GameEngine.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/GameEngine.java
@@ -12,11 +12,13 @@ public class GameEngine {
     }
 
     public void launchGame(){
+    //On boucle sur le jeu
         int indexPlayer = 0;
         int nbTour = 1;
         while(true){
             System.out.println("<       > Tour numero : "+nbTour+" <       >");
             this.playerList.get(indexPlayer).play();
+            //On vérifie si un joueur atteint le nombre max de points
             if (this.playerList.get(indexPlayer).getPoint() >= 9){
                 printWinner(this.playerList.get(indexPlayer));
                 break;
