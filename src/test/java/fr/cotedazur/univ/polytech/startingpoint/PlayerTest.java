e850c429a0a7c981542fc8649fd3d1212f445c69
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-01-07 13:44:00 +0100

    Bots can now pick cards

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PlayerTest.java
@@ -0,0 +1,90 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+
+    @Test
+    void getPoint() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        assertEquals(0,bot1.getPoint());
+    }
+
+    @Test
+    void setPoint() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.setPoint(15);
+        assertEquals(15,bot1.getPoint());
+    }
+
+    @Test
+    void getNom() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        assertEquals("bot1",bot1.getNom());
+    }
+
+    @Test
+    void setNom() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.setNom("botte1");
+        assertEquals("botte1",bot1.getNom());
+    }
+
+    @Test
+    void getNbActions() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        assertEquals(2,bot1.getNbActions());
+    }
+
+    @Test
+    void playAction() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.playAction();
+        assertEquals(1,bot1.getNbActions());
+    }
+
+    @Test
+    void resetNbActions() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.playAction();
+        bot1.resetNbActions();
+        assertEquals(2,bot1.getNbActions());
+    }
+
+    @Test
+    void resetNbBamboo() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.upNbBamboo(); bot1.upNbBamboo();
+        bot1.resetNbBamboo();
+        assertEquals(0,bot1.getNbBamboo());
+    }
+
+    @Test
+    void getNbBamboo() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        assertEquals(0,bot1.getNbBamboo());
+    }
+
+    @Test
+    void upNbBamboo() {
+        Board board = new Board();
+        Player bot1 = new Player(board,"bot1");
+        bot1.upNbBamboo();
+        assertEquals(1,bot1.getNbBamboo());
+    }
+}
