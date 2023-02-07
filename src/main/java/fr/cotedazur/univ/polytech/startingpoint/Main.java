d2d5d7bcc4a91148cb69e8f4c5dcc86ccc0103e4
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2023-02-07 09:52:54 +0100

    #89 logger now reacts to args in the main

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -8,7 +8,27 @@ import java.util.List;
 public class Main {
     public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void main(String... args) {
-        LOGGER.setLevel(Level.SEVERE);
+        System.out.println(args[0]);
+        if (args.length > 0) {
+            if (args[0].equals("--demo")) {
+                System.out.println("mode de démo");     //affichage complet
+            }
+
+            if (args[0].equals("--2thousands")) {
+                System.out.println("mode statistiques");    //aucun affichage sauf à la fin des 1000 parties
+                LOGGER.setLevel(Level.SEVERE);
+            }
+            if (args[0].equals("--csv")) {
+                System.out.println("mode csv");    //aucun affichage sauf à la fin des parties
+                LOGGER.setLevel(Level.SEVERE);
+            }
+        } else {
+            LOGGER.setLevel(Level.SEVERE);
+        }
+
+
+
+
         Board board = new Board();
 
         Bot bot1 = new PrimaryBot(board, "Simon");
