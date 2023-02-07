3f7e2a28f58a7e8cbc9430c45b8f0640dbf4388a
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-02-07 11:04:24 +0100

    création dans un dossier stats du fichier gamestats.csv qui contient les infos des jouers dans une partie #94

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,14 +1,35 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 import fr.cotedazur.univ.polytech.startingpoint.bots.Bot;
 import fr.cotedazur.univ.polytech.startingpoint.bots.PrimaryBot;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.*;
 import java.util.List;
+import com.opencsv.*;
+import java.io.FileReader;
+import java.net.URL;
 
 public class Main {
     public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    public static void main(String... args) {
-        LOGGER.setLevel(Level.INFO);
+    static File file = new File("stats/test.csv");
+    public static void main(String... args) throws IOException {
+
+        String file = "stats/gamestats.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(file));
+
+        //Créer un tableau de chaînes pour les en-têtes de colonne
+        String[] headers = {"Nom", "Victoires", "NombreParties"};
+
+        //Écrire les en-têtes de colonne dans le fichier
+        writer.writeNext(headers);
+
+
+
+
+        LOGGER.setLevel(Level.SEVERE);
         Board board = new Board();
 
         Bot bot1 = new PrimaryBot(board, "Simon");
@@ -20,6 +41,12 @@ public class Main {
         listPlayer.add(bot2);
         GameEngine game = new GameEngine(board, listPlayer);
         game.launchGame();
+        String[] bot1Info = {bot1.getNom(),""+bot1.getPoint(),"0"};
+        String[] bot2Info = {bot2.getNom(),""+bot2.getPoint(),"1"};
+        writer.writeNext(bot1Info);
+        writer.writeNext(bot2Info);
+        writer.close();
+
 
     }
 }
