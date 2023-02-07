d2fb05f62722a16a0d4f4dcad18d200e971ffa1c
Vranox <damyscus@gmail.com>
2023-02-07 14:31:32 +0100

    Les bots peuvent maintenant détecter des patterns de type boomrang et triangle

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/bots/SkynetBot.java
@@ -0,0 +1,10 @@
+package fr.cotedazur.univ.polytech.startingpoint.bots;
+
+import fr.cotedazur.univ.polytech.startingpoint.Board;
+
+public class SkynetBot extends Bot {
+    public SkynetBot(Board board, String nom) {
+        super(board, nom);
+    }
+
+}
