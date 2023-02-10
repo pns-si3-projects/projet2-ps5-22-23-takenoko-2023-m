73eb8cab987d17b37e5ed672a312e8e3b56085e4
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-02-10 10:46:02 +0100

    Documentation for the bots #139

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/bots/LittleBot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/bots/LittleBot.java
@@ -6,14 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LittleBot extends IntermediateBot{
-
+    /** The card to focus during the gameplay*/
     private ObjectiveInterface focusCard = null;
     int nbTour = 1;
 
+    /**
+     * The constructor of the LittleBot
+     * @param board The board of the LittleBot
+     * @param nom The name of the LittleBot
+     */
     public LittleBot(Board board, String nom) {
         super(board, nom);
     }
 
+    /**
+     * The main method bot, it will be called every tour
+     */
     public void play(){
         this.resetNbActions();
         this.objectives.sort((o1, o2) -> o2.getNbPointsWin() - o1.getNbPointsWin());
@@ -51,40 +59,66 @@ public class LittleBot extends IntermediateBot{
         nbTour++;
     }
 
+    /**This method will play when the dice is Lightning*/
     public void playLigntningDice(){
         super.playLigntningDice();
     }
 
+    /**This method will play when the dice is Cloud*/
     public void playCloudDice(){
         super.playCloudDice();
     }
 
+    /**
+     * This method will play the bot when the focus card is ObjectiveGardener
+     */
     public void playForGardenerCard(){
       super.playForGardenerCard();
     }
 
+    /**
+     * This method will play the bot when the focus card is ObjectivePanda
+     */
     public void playForPandaCard(){
         super.playForPandaCard();
     }
 
+    /**
+     * This method will play the bot when the focus card is ObjectivePlot
+     */
     public void playForPatternCard(){
         super.playForPatternCard();
     }
 
-
+    /**
+     * This method will choose the best of the 3 cards in function of the focusCard
+     * @param threeCards A List of the 3 cards
+     * @return The best of the 3 cards
+     */
     public Tile chooseBetterOf3Tiles(List<Tile> threeCards){
       return super.chooseBetterOf3Tiles(threeCards);
 
     }
-
+    /**
+     * This method will find the first ObjectiveGardener card in the bot's hand
+     * @return The first ObjectiveGardener's card
+     */
     public ObjectiveGardener findObjectiveGardener(){
        return super.findObjectiveGardener();
     }
 
+    /**
+     * This method will find the first ObjectivePanda card in the bot's hand
+     * @return The first ObjectivePanda's card
+     */
     public ObjectivePanda findObjectivePanda(){
        return super.findObjectivePanda();
     }
 
+    /**
+     * This method will find the first ObjectivePlot card in the bot's hand
+     * @return The first ObjectivePlot's card
+     */
     public ObjectivePlot findObjectivePlot(){
        return super.findObjectivePlot();
     }
